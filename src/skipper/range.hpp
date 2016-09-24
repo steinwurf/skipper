516dde8b49f3a8096b48d27990a751732acad76b
Peter Vingelmann <petya@master.com>
2016-09-24 03:58:44 +0200

    No indent for namespaces

--- src/skipper/range.hpp
+++ src/skipper/range.hpp
@@ -12,35 +12,35 @@
 namespace skipper
 {
 
-    /// validates that the provided value is within a defined range
-    template<typename Type>
-    struct range
+/// validates that the provided value is within a defined range
+template<typename Type>
+struct range
+{
+    range(Type lower, Type upper) :
+        m_lower(lower), m_upper(upper)
     {
-        range(Type lower, Type upper) :
-            m_lower(lower), m_upper(upper)
-        {
-            assert(lower <= upper);
-        }
+        assert(lower <= upper);
+    }
 
-        /// @copydoc any::operator()
-        bool operator()(const Type value) const
-        {
-            return ((value >= m_lower) && (value <= m_upper));
-        }
+    /// @copydoc any::operator()
+    bool operator()(const Type value) const
+    {
+        return ((value >= m_lower) && (value <= m_upper));
+    }
 
-        /// @copydoc any::operator<<
-        friend std::ostream& operator<<(std::ostream& os, const range& r)
-        {
-            return os << "of type " << typeid(Type).name() << " in ["
-                   << r.m_lower << "," << r.m_upper << "]";
-        }
+    /// @copydoc any::operator<<
+    friend std::ostream& operator<<(std::ostream& os, const range& r)
+    {
+        return os << "of type " << typeid(Type).name() << " in ["
+               << r.m_lower << "," << r.m_upper << "]";
+    }
 
-    private:
+private:
 
-        /// the lower limit in the range
-        const Type m_lower;
+    /// the lower limit in the range
+    const Type m_lower;
 
-        /// the upper limit in the range
-        const Type m_upper;
-    };
+    /// the upper limit in the range
+    const Type m_upper;
+};
 }
--- src/skipper/set.hpp
+++ src/skipper/set.hpp
@@ -13,37 +13,37 @@
 namespace skipper
 {
 
-    /// validates that the provided value is a set
-    template<typename Type>
-    struct set
+/// validates that the provided value is a set
+template<typename Type>
+struct set
+{
+    set(const std::initializer_list<Type> values) :
+        m_values(values)
+    {}
+
+    /// @copydoc any::operator()
+    bool operator()(Type value) const
     {
-        set(const std::initializer_list<Type> values) :
-            m_values(values)
-        {}
+        return (std::find(m_values.begin(), m_values.end(), value) !=
+                m_values.end());
+    }
 
-        /// @copydoc any::operator()
-        bool operator()(Type value) const
+    /// @copydoc any::operator<<
+    friend std::ostream& operator<<(std::ostream& os, const set& s)
+    {
+        os << "of type " << typeid(Type).name() << " in {";
+        for (auto i = s.m_values.begin(); i != s.m_values.end(); i++)
         {
-            return (std::find(m_values.begin(), m_values.end(), value) !=
-                    m_values.end());
+            if (i != s.m_values.begin())
+                os << ",";
+            os << *i;
         }
+        return os << "}";
+    }
 
-        /// @copydoc any::operator<<
-        friend std::ostream& operator<<(std::ostream& os, const set& s)
-        {
-            os << "of type " << typeid(Type).name() << " in {";
-            for (auto i = s.m_values.begin(); i != s.m_values.end(); i++)
-            {
-                if (i != s.m_values.begin())
-                    os << ",";
-                os << *i;
-            }
-            return os << "}";
-        }
+private:
 
-    private:
-
-        /// the values in the set
-        const std::vector<Type> m_values;
-    };
+    /// the values in the set
+    const std::vector<Type> m_values;
+};
 }
